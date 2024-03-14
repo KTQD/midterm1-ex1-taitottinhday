1feecd3a898f64972af4296ad2f825954e73b119
taitottinhday <145837513+taitottinhday@users.noreply.github.com>
2024-03-14 08:07:16 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,39 @@
-public class Book {
+public class Book extends Item {
+  3 usages
+  private String author;
+  3 usages
+  private int numberOfPages;
+  3 usages
+  private String genre;
+  1 usages
+  public Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+  no usages
+  public String getAuthor() {
+        return author;
+  }
+  no usages
+  public void setAuthor(String author) {
+        this.author = author;
+    }
+  no usages
+  public int getNumberOfPages() {
+        return numberOfPages;
+    }
+  no usages
+ public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+  no usages
+   public String getGenre() {
+        return genre;
+    }
+  no usages
+  public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
