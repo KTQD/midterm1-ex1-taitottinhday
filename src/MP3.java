50ba2976c30e00282f2de63f6d2fa20a62a5286d
taitottinhday <145837513+taitottinhday@users.noreply.github.com>
2024-03-14 08:01:39 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,19 @@
-public class MP3 {
+public class MP3 extends Item {
+  3 usages
+  private int duration;
+  1 usages
+  public MP3(String name, String description, String ID, float price, int duration) {
+      super(name, description, ID, price);
+      this.duration = duration;
+  }
+ no usages
+    public int getDuration() {
+        return duration;
+    }
+no usages
+  public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
+
 }
