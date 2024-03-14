49271c711fa4168d49fc54831f527b4918bb67d6
taitottinhday <145837513+taitottinhday@users.noreply.github.com>
2024-03-14 08:14:03 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("cheer up", "dsadas", "123456", 11.1f, "344", 180);
+        System.out.println("MP3 Information:");
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("caycamngot", "asdsad", "789012", 29.99f, "fdssd", 300, "Fiction");
+        System.out.println("\nBook Information:");
         book.showInfo();
+       
 
     }
 }
